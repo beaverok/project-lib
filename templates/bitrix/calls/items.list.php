7a5a29ef04ead0d1bdb5b03c75866002a8365f01
beaverok <askgroup77@gmail.com>
2016-09-08 15:10:11 +0500

    add new component

---
+++ templates/bitrix/calls/items.list.php
@@ -0,0 +1,14 @@
+<?$APPLICATION->IncludeComponent(
+    "readys:items.list",
+    "",
+    Array(
+        "CHECK_DATES" => "Y",
+        "FIELD_CODE" => array("",""),
+        "IBLOCK_ID" => "5",
+        "IBLOCK_TYPE" => "info",
+        "NEWS_COUNT" => "4",
+        "PROPERTY_CODE" => array("LINK",""),
+        "SORT_BY1" => "NAME",
+        "SORT_ORDER1" => "ASC"
+    )
+);?>
